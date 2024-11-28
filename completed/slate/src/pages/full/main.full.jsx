6377cff80a29c7567e5f1f37193f1e6f0f6df662
Abhinav <abhinav@bitcomplete.io>
2024-11-28 14:11:49 -0800

    fix: remove compiled JS from src/
    Signed-off-by: Abhinav <abhinav@bitcomplete.io>

--- completed/slate/src/pages/full/main.full.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import '../../index.css';
-import RichTextExample from './editor.full';
-createRoot(document.getElementById('root')).render(<StrictMode>
-        <div className="container">
-            <RichTextExample />
-        </div>
-    </StrictMode>);
