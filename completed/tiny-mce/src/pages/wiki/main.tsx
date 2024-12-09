39d79dd2655293117fb9459c3cff3f891668035e
Abhinav <abhinav@bitcomplete.io>
2024-12-08 21:33:02 -0800

    feat: add variants to TinyMCE - basic, full, wiki
    Signed-off-by: Abhinav <abhinav@bitcomplete.io>

---
+++ completed/tiny-mce/src/pages/wiki/main.tsx
@@ -0,0 +1,46 @@
+import { StrictMode, useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { createRoot } from 'react-dom/client'
+
+export default function App() {
+    const editorRef = useRef<Editor>(null)
+
+    return (
+        <>
+            <Editor
+                apiKey="2irvdgkv2qh7wlkymza35kf8pvw7wzwikexz0t9j479b3bss"
+                // onInit={}
+                initialValue="<p>This is the initial content of the editor.</p>"
+                ref={editorRef}
+                init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                        'autolink',
+                        'lists',
+                        'link',
+                        'preview',
+                        'anchor',
+                        'code',
+                        'fullscreen',
+                        'table',
+                        'help',
+                        'wordcount',
+                    ],
+                    toolbar:
+                        'undo redo | blocks | ' +
+                        'bold italic | bullist numlist | ' +
+                        'removeformat | help',
+                    content_style:
+                        'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+                }}
+            />
+        </>
+    )
+}
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
--- completed/tiny-mce/vite.config.ts
+++ completed/tiny-mce/vite.config.ts
@@ -1,8 +1,36 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { getHtmlFiles } from '../../utils/vite-helper';
+
+const editorPages = getHtmlFiles('./src/pages');
+
+console.debug("Pages to be built: ", editorPages);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  root: './src',
+  base: "",
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: editorPages,
+      output: {
+        dir: 'dist',
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          'tinymce': ['@tinymce/tinymce-react'],
+        }
+      }
+    },
+  },
+  server: {
+    open: './pages/wiki/index.html',
+  },
 })
