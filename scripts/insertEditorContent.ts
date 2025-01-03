fa4f18d23e3587d3c177e168f47a5d7f0ef7f97f
Mikael Gramont <mgramont@gmail.com>
2025-01-03 13:44:08 +0100

    Updated index.html for all editors, now using #editor-content-json

--- scripts/insertEditorContent.ts
+++ scripts/insertEditorContent.ts
@@ -9,7 +9,7 @@ const htmlFiles = getHtmlFiles(`${process.cwd()}/src`);
 const itemsPath = fs.realpathSync(`${process.cwd()}/../../items.json`);
 const items = JSON.parse(fs.readFileSync(itemsPath, 'utf-8'));
 
-Object.entries(htmlFiles).forEach(([path, file]) => {
+function updateEditorContent(file: string, items: any[]) {
   const content = fs.readFileSync(file, 'utf8');
   const item = items.find((item) =>  item.attributes.directorySlug === editorName);
   const newContent = item.copy?.editorContent;
@@ -17,7 +17,6 @@ Object.entries(htmlFiles).forEach(([path, file]) => {
     console.error(`No editor content found for ${file}`);
     return;
   }
-  
   // const variantName = path.split('/').pop();
   // const variant = item.attributes.variants.find(({ variantKey }) => variantKey === variantName);
 
@@ -29,7 +28,7 @@ Object.entries(htmlFiles).forEach(([path, file]) => {
     throw error;
   }
 
-  const editorContent = root.querySelector('#editor-content');
+  const editorContent = root.querySelector('#editor-content-json');
   if (!editorContent) {
     console.error(`No editor content container found in ${file}`);
     return;
@@ -42,4 +41,14 @@ Object.entries(htmlFiles).forEach(([path, file]) => {
     console.error(`Error writing ${file} ${editorName}`, error);
     throw error;
   }
-})
+}
+
+if (Object.values(htmlFiles).length > 0) {
+  Object.entries(htmlFiles).forEach(([path, file]) => {
+    updateEditorContent(file, items);
+  })
+} else {
+  // No variants, process just one file
+  const file = `${process.cwd()}/index.html`
+  updateEditorContent(file, items);
+}
