e880be57d40946c9fcfb19c253b066e9c24044b0
Mikael Gramont <mgramont@gmail.com>
2025-01-20 08:54:30 +0100

    Enforce default font on editor body's

---
+++ completed/editor-js/dist/assets/basic-dfshadHl.js
@@ -0,0 +1,1 @@
+import{T as t,d as e,a as o,s,S as r,p as a}from"./style-BsJtlLTO.js";new t({holder:"editorjs",autofocuse:!0,tools:{list:{class:e,inlineToolbar:!0,config:{defaultStyle:"unordered"}},underline:o,inlineCode:{class:s,shortcut:"CMD+SHIFT+M"},strikethrough:r},data:{blocks:a()}});
