16beb0d6c0ce1a3cd7ba91fe99978cfe2c12845e
Mikael Gramont <mgramont@gmail.com>
2025-01-23 16:12:10 +0100

    TinyMCE AI variant

--- completed/tiny-mce/src/pages/ai/main.tsx
+++ completed/tiny-mce/src/pages/ai/main.tsx
@@ -3,8 +3,8 @@ import { StrictMode, useRef } from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import { createRoot } from 'react-dom/client'
 import { parseContentToHTML } from '../../../../../scripts/parseContentToHtml'
-import API_KEY from '../../api-key'
 import helpers from '../../helpers'
+import API_KEY from '../../api-key'
 
 export default function App() {
     const editorRef = useRef<Editor>(null)
@@ -41,7 +41,10 @@ export default function App() {
                         'importword',
                         'exportword',
                         'exportpdf',
-                        'autoresize',
+                        'ai',
+                        'preview',
+                        'powerpaste',
+                        'revisionhistory',
                     ],
                     toolbar:
                         'undo redo | blocks | ' +
@@ -49,29 +52,31 @@ export default function App() {
                         'alignright alignjustify | bullist numlist outdent indent | ' +
                         'importword exportword exportpdf |' +
                         'revisionhistory |' +
+                        'aidialog aishortcuts |' +
                         'removeformat | help',
                     content_style:
                         'body { font-family:Helvetica,Arial,sans-serif; font-size:14px } a { color: #0969da }',
                     exportpdf_converter_options: {
-                        format: 'Letter',
-                        margin_top: '1in',
-                        margin_right: '1in',
-                        margin_bottom: '1in',
-                        margin_left: '1in',
-                    },
+                        'format': 'Letter',
+                        'margin_top': '1in',
+                        'margin_right': '1in',
+                        'margin_bottom': '1in',
+                        'margin_left': '1in'
+                        },
                     exportword_converter_options: {
                         document: {
                             size: 'Letter',
                         },
                     },
                     importword_converter_options: {
-                        formatting: {
-                            styles: 'inline',
-                            resets: 'inline',
-                            defaults: 'inline',
-                        },
+                        'formatting': {
+                            'styles': 'inline',
+                            'resets': 'inline',
+                            'defaults': 'inline',
+                        }
                     },
                     revisionhistory_fetch: helpers.revisionhistory_fetch,
+                    ai_request: helpers.ai_request,
                 }}
             />
         </>
